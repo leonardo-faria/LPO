e680aebabbf2a261bfc1d4e29efc786c70f5c087
ZPaulo <jose.p.s.olive@gmail.com>
2014-06-05 03:36:12 +0100

    Animations
    Signed-off-by: ZPaulo <jose.p.s.olive@gmail.com>

--- my-gdx-game-core/src/com/Lpoo/game/MainMenu.java
+++ my-gdx-game-core/src/com/Lpoo/game/MainMenu.java
@@ -1,5 +1,9 @@
 package com.Lpoo.game;
 
+import aurelienribon.tweenengine.Timeline;
+import aurelienribon.tweenengine.Tween;
+import aurelienribon.tweenengine.TweenManager;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
@@ -7,6 +11,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -26,13 +31,15 @@ public class MainMenu implements Screen {
 	private TextButton exitButton, startButton;
 	private BitmapFont white;
 	private Label heading;
+	private TweenManager tweenManager;
 
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		
+
+		tweenManager.update(delta);
+
 		stage.act(delta);
 		stage.draw();
 	}
@@ -45,9 +52,9 @@ public class MainMenu implements Screen {
 	@Override
 	public void show() {
 		stage = new Stage();
-		
+
 		Gdx.input.setInputProcessor(stage);
-		
+
 		atlas = new TextureAtlas("ui/button.pack");
 		skin = new Skin(atlas);
 		table = new Table(skin);
@@ -72,7 +79,7 @@ public class MainMenu implements Screen {
 			}
 		});
 		exitButton.pad(10);
-		
+
 		startButton = new TextButton("START", textButtonStyle);
 		startButton.addListener(new ClickListener(){
 			@Override
@@ -81,21 +88,50 @@ public class MainMenu implements Screen {
 			}
 		});
 		startButton.pad(10);
-		
+
 		//Creating heading
-		LabelStyle headingStyle = new LabelStyle(white, Color.WHITE);
-			
-		heading = new Label(JumpEm.TITLE, headingStyle);
+		heading = new Label(JumpEm.TITLE, new LabelStyle(white, Color.WHITE));
 		heading.setFontScale(2);
 
 		table.add(heading);
-		table.row();
-		table.row();
+		table.getCell(heading).spaceBottom(100);
 		table.row();
 		table.add(startButton);
+		table.getCell(startButton).spaceBottom(15);
 		table.row();
 		table.add(exitButton);
 		stage.addActor(table);
+
+		//animations
+		tweenManager = new TweenManager();
+		Tween.registerAccessor(Actor.class, new ActorAccessor());
+
+		//pretty colours
+		Timeline.createSequence().beginSequence()
+		.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(0,0,1))
+		.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(0,1,0))
+		.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(1,0,0))
+		.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(1,1,0))
+		.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(0,1,1))
+		.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(1,0,1))
+		.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(1,1,1))
+		.end().repeat(Tween.INFINITY, 0).start(tweenManager);
+
+
+
+		//buttons fade in
+		Timeline.createSequence().beginSequence()
+		.push(Tween.set(startButton, ActorAccessor.ALPHA).target(0))
+		.push(Tween.set(exitButton, ActorAccessor.ALPHA).target(0))
+		.push(Tween.from(heading, ActorAccessor.ALPHA, .25f).target(0))
+		.push(Tween.to(startButton, ActorAccessor.ALPHA, .25f).target(1))
+		.push(Tween.to(exitButton, ActorAccessor.ALPHA, .25f).target(1))
+		.end().start(tweenManager);
+
+		//table fade in
+		Tween.from(table, ActorAccessor.ALPHA, .5f).target(0).start(tweenManager);
+		Tween.from(table, ActorAccessor.Y, .5f).target(Gdx.graphics.getHeight()/8).start(tweenManager);
+
 	}
 
 	@Override
@@ -115,6 +151,10 @@ public class MainMenu implements Screen {
 
 	@Override
 	public void dispose() {
+		stage.dispose();
+		atlas.dispose();
+		skin.dispose();
+		white.dispose();
 
 	}
 
