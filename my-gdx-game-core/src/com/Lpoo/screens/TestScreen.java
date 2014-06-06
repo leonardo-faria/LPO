62ba91141f703a08e8570162ddd2565b2cd3495a
ZPaulo <jose.p.s.olive@gmail.com>
2014-06-06 17:00:56 +0100

    sd
    Signed-off-by: ZPaulo <jose.p.s.olive@gmail.com>

--- my-gdx-game-core/src/com/Lpoo/screens/TestScreen.java
+++ my-gdx-game-core/src/com/Lpoo/screens/TestScreen.java
@@ -1,5 +1,10 @@
 package com.Lpoo.screens;
 
+import com.Lpoo.game.JumpEmCollision;
+import com.Lpoo.game.JumpEmInputProcessor;
+import com.Lpoo.game.Jumper;
+import com.Lpoo.game.Trampoline;
+import com.Lpoo.game.Wall;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
