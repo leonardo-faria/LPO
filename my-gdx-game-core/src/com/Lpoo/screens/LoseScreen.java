179b5561340553a2dc4ff3149555fab4a3587193
leonardo-faria <leonardofaria94@gmail.com>
2014-06-07 16:13:47 +0100

    score and time
    Signed-off-by: leonardo-faria <leonardofaria94@gmail.com>

--- my-gdx-game-core/src/com/Lpoo/screens/LoseScreen.java
+++ my-gdx-game-core/src/com/Lpoo/screens/LoseScreen.java
@@ -24,7 +24,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
-public class LoseScreen implements Screen{
+public class LoseScreen implements Screen {
 
 	private Stage stage;
 	private TextureAtlas atlas;
@@ -32,7 +32,7 @@ public class LoseScreen implements Screen{
 	private Table table;
 	private TextButton returnButton;
 	private BitmapFont white;
-	private Label heading, timeGame ,points;
+	private Label heading, timeGame, points;
 	private TweenManager tweenManager;
 
 	@Override
@@ -53,7 +53,10 @@ public class LoseScreen implements Screen{
 
 	@Override
 	public void show() {
-
+		if(JumpEm.bestScore < JumpEm.lastScore)
+			JumpEm.bestScore = JumpEm.lastScore;
+		if(JumpEm.BestTime < JumpEm.lastTime)
+			JumpEm.BestTime = JumpEm.lastTime;
 		stage = new Stage();
 
 		Gdx.input.setInputProcessor(stage);
@@ -65,7 +68,7 @@ public class LoseScreen implements Screen{
 
 		white = new BitmapFont(Gdx.files.internal("font/white.fnt"), false);
 
-		//creating buttons
+		// creating buttons
 		TextButtonStyle textButtonStyle = new TextButtonStyle();
 		textButtonStyle.up = skin.getDrawable("wood");
 		textButtonStyle.down = skin.getDrawable("wood");
@@ -74,11 +77,12 @@ public class LoseScreen implements Screen{
 		textButtonStyle.font = white;
 		textButtonStyle.fontColor = Color.WHITE;
 
-		returnButton = new TextButton("Accept", textButtonStyle );
-		returnButton.addListener(new ClickListener(){
+		returnButton = new TextButton("Accept", textButtonStyle);
+		returnButton.addListener(new ClickListener() {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
-				((Game) Gdx.app.getApplicationListener()).setScreen(new MainMenu());
+				((Game) Gdx.app.getApplicationListener())
+						.setScreen(new MainMenu());
 			}
 		});
 
@@ -87,9 +91,12 @@ public class LoseScreen implements Screen{
 		heading = new Label("SCORE", new LabelStyle(white, Color.WHITE));
 		heading.setFontScale(2);
 
-		timeGame = new Label("Total Play Time: " + JumpEm.lastTime, new LabelStyle(white, Color.WHITE));
+		timeGame = new Label("Total Play Time: " + JumpEm.lastTime
+				/ 1000 + ":" + JumpEm.lastTime% 1000,
+				new LabelStyle(white, Color.WHITE));
 
-		points = new Label("Points Acumulated: " + JumpEm.lastScore, new LabelStyle(white, Color.WHITE));
+		points = new Label("Points Acumulated: " + JumpEm.lastScore,
+				new LabelStyle(white, Color.WHITE));
 
 		table.add(heading);
 		table.getCell(heading).spaceBottom(100);
@@ -103,35 +110,39 @@ public class LoseScreen implements Screen{
 		table.add(returnButton);
 		stage.addActor(table);
 
-		//animations
+		// animations
 		tweenManager = new TweenManager();
 		Tween.registerAccessor(Actor.class, new ActorAccessor());
 
-		//pretty colours
-		Timeline.createSequence().beginSequence()
-		.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(0,0,1))
-		.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(0,1,0))
-		.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(1,0,0))
-		.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(1,1,0))
-		.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(0,1,1))
-		.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(1,0,1))
-		.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(1,1,1))
-		.end().repeat(Tween.INFINITY, 0).start(tweenManager);
+		// pretty colours
+		Timeline.createSequence()
+				.beginSequence()
+				.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(0, 0, 1))
+				.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(0, 1, 0))
+				.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(1, 0, 0))
+				.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(1, 1, 0))
+				.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(0, 1, 1))
+				.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(1, 0, 1))
+				.push(Tween.to(heading, ActorAccessor.RGB, .5f).target(1, 1, 1))
+				.end().repeat(Tween.INFINITY, 0).start(tweenManager);
 
-		//buttons fade in
-		Timeline.createSequence().beginSequence()
-		.push(Tween.set(timeGame, ActorAccessor.ALPHA).target(0))
-		.push(Tween.set(points, ActorAccessor.ALPHA).target(0))
-		.push(Tween.set(returnButton, ActorAccessor.ALPHA).target(0))
-		.push(Tween.from(heading, ActorAccessor.ALPHA, .25f).target(0))
-		.push(Tween.to(timeGame, ActorAccessor.ALPHA, .25f).target(1))
-		.push(Tween.to(points, ActorAccessor.ALPHA, .25f).target(1))
-		.push(Tween.to(returnButton, ActorAccessor.ALPHA, .25f).target(1))
-		.end().start(tweenManager);
+		// buttons fade in
+		Timeline.createSequence()
+				.beginSequence()
+				.push(Tween.set(timeGame, ActorAccessor.ALPHA).target(0))
+				.push(Tween.set(points, ActorAccessor.ALPHA).target(0))
+				.push(Tween.set(returnButton, ActorAccessor.ALPHA).target(0))
+				.push(Tween.from(heading, ActorAccessor.ALPHA, .25f).target(0))
+				.push(Tween.to(timeGame, ActorAccessor.ALPHA, .25f).target(1))
+				.push(Tween.to(points, ActorAccessor.ALPHA, .25f).target(1))
+				.push(Tween.to(returnButton, ActorAccessor.ALPHA, .25f).target(
+						1)).end().start(tweenManager);
 
-		//table fade in
-		Tween.from(table, ActorAccessor.ALPHA, .5f).target(0).start(tweenManager);
-		Tween.from(table, ActorAccessor.Y, .5f).target(Gdx.graphics.getHeight()/8).start(tweenManager);
+		// table fade in
+		Tween.from(table, ActorAccessor.ALPHA, .5f).target(0)
+				.start(tweenManager);
+		Tween.from(table, ActorAccessor.Y, .5f)
+				.target(Gdx.graphics.getHeight() / 8).start(tweenManager);
 	}
 
 	@Override
